278c7ebda7898876d70691014b1bdaeb59ca1f30
fikridermawan <fikryd112@gmail.com>
2022-11-03 12:56:44 +0700

    first commit

---
+++ app/src/main/java/com/project/toptalent/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.project.toptalent.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.toptalent.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
